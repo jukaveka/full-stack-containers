66fc3055b9343490d5352e8b35fba1ba53e454ba
jukaveka <juhokavekari@hotmail.com>
2026-06-23 19:02:30 +0300

    Exercise 12.21 - Inspect backend container

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,7 +1,5 @@
 require("dotenv").config()
 
-console.log(process.env.NODE_ENV)
-
 const MONGO_URL = process.env.NODE_ENV = "production"
                     ? process.env.MONGO_URL || undefined
                     : undefined
@@ -10,6 +8,6 @@ const REDIS_URL = process.env.NODE_ENV = "production"
                     : undefined
 
 module.exports = {
-  MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
-  REDIS_URL//: '//localhost:6378'
+  MONGO_URL,
+  REDIS_URL
 }
